65bd75b6362422744aed4217ab339bf3f80efe73
terakoya76 <terako.studio@gmail.com>
2022-02-24 23:50:18 +0900

    test: add unittest for evaluator

---
+++ evaluator/evaluator_test.go
@@ -0,0 +1,33 @@
+package evaluator_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/terakoya76/modd/evaluator"
+)
+
+func TestGetIdentifiersFromMaaping(t *testing.T) {
+	cases := []struct {
+		name     string
+		mapping  map[string][]string
+		expected []string
+	}{
+		{
+			name: "when scope is wildcard without tags",
+			mapping: map[string][]string{
+				"foo": {"a", "b"},
+				"bar": {"c", "d"},
+			},
+			expected: []string{"foo", "bar"},
+		},
+	}
+
+	for _, c := range cases {
+		actual := evaluator.GetIdentifiersFromMaaping(c.mapping)
+		if !assert.ElementsMatch(t, c.expected, actual) {
+			t.Errorf("case: %s is failed, expected: %+v, actual: %+v\n", c.name, c.expected, actual)
+		}
+	}
+}
